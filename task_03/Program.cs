ff4d8dd1441fe88661a8f49dff58b63dda9416c8
Andrey <rsdak@ksc.krasn.ru>
2023-11-22 10:00:39 +0700

    add task 3

---
+++ task_03/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+
+int[] CreateArray(string message = "Введите числа массива через запятую")
+{
+    Console.WriteLine(message);
+    string input = Console.ReadLine();
+    string[] arrayString = input.Split(",");
+    int[] array = new int[arrayString.Length];
+    for (int i = 0; i < arrayString.Length; i++)
+    {
+        array[i] = Convert.ToInt32(arrayString[i]);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{String.Join(" ", array)}]");
+}
+
+void ReverseArrey(int[] array, int start = 0)
+{
+    int end = array.Length - 1 - start;
+    if (start >= end)
+    {
+        return;
+    }
+    int temp = array[start];
+    array[start] = array[end];
+    array[end] = temp; 
+    ReverseArrey(array, start + 1);
+}
+
+
+int[] newArray = CreateArray();
+PrintArray(newArray);
+ReverseArrey(newArray);
+PrintArray(newArray);
